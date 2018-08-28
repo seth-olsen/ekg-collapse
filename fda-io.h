62f60b20ae27ddb607f785dd01b6029e75cd2d20
Seth Olsen <setholsenr@gmail.com>
2018-08-28 09:52:18 -0400

    added parameter print function and updated petsc makefiles

--- fda-io.h
+++ fda-io.h
@@ -42,6 +42,25 @@ void read_step(char *files[], int times[], double *fields[], int nfields) {
   return;
 }
 
-
-
+string param_print(string outfile, int lastpt, int save_pt = 1, int nsteps,
+		   int save_step = 8, double lam = 0.25, double r2m,
+		   double rmin, double rmax, double dspn, double tol, int maxit,
+		   double ic_Dsq, double ic_r0, double ic_Amp, int check_step,
+		   bool zero_pi, bool somm_cond = true, bool dspn_bound) {
+  string z_p = (zero_pi) ? "true" : "false";
+  string s_c = (somm_cond) ? "true" : "false";
+  string d_b = (dspn_bound) ? "true" : "false";
+  return "\noutfile name = " + outfile + "\ngrid size = " +
+    to_string(lastpt) + " (" + to_string(save_pt) + "/write)\ntime steps = " +
+    to_string(nsteps) + " (" + to_string(save_step) + "/write)\nlambda = " +
+    to_string(lam) + "\nr2m = " + to_string(r2m) + "\nrmin = " + to_string(rmin)
+    + "\nrmax = " + to_string(rmax) + "\ndissipation = " + to_string(dspn) +
+    "\niterative tolerance = " + to_string(tol) + "\nmaximum iterations = " +
+    to_string(maxit) + "\nic_Dsq = " + to_string(ic_Dsq) + "\nic_r0 = " +
+    to_string(ic_r0) + "\nic_Amp = " + to_string(ic_Amp) + "\nmass check step = "
+    + to_string(check_step) + "\nmaximum evolution time = " + to_string(nsteps*dt)
+    + "\ndr = " + to_string(dr) + "\ndt = " + to_string(dt) +
+    "\n\noptions:\nzero pi_0 = " + z_p + "\nsommerfeld bc = " + s_c +
+    "\ndissipation at bound = " + d_b + "\n";
+}
 
